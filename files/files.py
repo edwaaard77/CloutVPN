fda70a27de3dd8109b79e9ea564018be6c7e80ff
edwaaard77 <eduard.glazyrin@hotmail.com>
2025-01-17 22:42:34 +0300

    first commit

--- files/files.py
+++ files/files.py
@@ -1,9 +1,8 @@
 from aiogram import types
 
 nekobox_windows = types.FSInputFile(
-    path="C:/Users/Эдуард/Desktop/CloutVPN/CloutVPN_bot/files/nekoray-3.26-2023-12-09-windows64.zip"
+    path="/app/files/nekoray-3.26-2023-12-09-windows64.zip"
 )
-
 nekobox_android = types.FSInputFile(
-    path="C:/Users/Эдуард/Desktop/CloutVPN/CloutVPN_bot/files/NB4A-1_3_2-arm64-v8a.apk"
+    path="/app/files/NB4A-1_3_2-arm64-v8a.apk"
 )
