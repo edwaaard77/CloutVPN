474dc62afe89d7c297741a39a61c35925a72e1a3
edwaaard77 <eduard.glazyrin@hotmail.com>
2025-02-03 23:56:10 +0300

    docker

--- files/files.py
+++ files/files.py
@@ -1,7 +1,7 @@
 from aiogram import types
 
 nekobox_windows = types.FSInputFile(
-    path="/root/CloutVPN/files/nekoray-3.26-2023-12-09-windows64.zip"
+    path="files/nekoray-3.26-2023-12-09-windows64.zip"
 )
 nekobox_android = types.FSInputFile(
     path="/root/CloutVPN/files/NB4A-1_3_2-arm64-v8a.apk"
